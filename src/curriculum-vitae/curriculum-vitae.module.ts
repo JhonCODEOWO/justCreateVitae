b2044de85ddce39b2aec11997322b7502cf200c8
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-03-25 10:51:22 -0600

    feat: add ImageManagementModule and integrate image processing in CurriculumVitaeService.

--- src/curriculum-vitae/curriculum-vitae.module.ts
+++ src/curriculum-vitae/curriculum-vitae.module.ts
@@ -3,10 +3,11 @@ import { CurriculumVitaeService } from './curriculum-vitae.service';
 import { CurriculumVitaeController } from './curriculum-vitae.controller';
 import { TemplatesModule } from 'src/templates/templates.module';
 import { PdfModule } from 'src/pdf/pdf.module';
+import { ImageManagementModule } from 'src/image-management/image-management.module';
 
 @Module({
   controllers: [CurriculumVitaeController],
   providers: [CurriculumVitaeService],
-  imports: [TemplatesModule, PdfModule],
+  imports: [TemplatesModule, PdfModule, ImageManagementModule],
 })
 export class CurriculumVitaeModule {}
--- src/curriculum-vitae/curriculum-vitae.service.ts
+++ src/curriculum-vitae/curriculum-vitae.service.ts
@@ -5,12 +5,14 @@ import { MapperTemplateData } from 'src/templates/mapper';
 import { TemplatesService } from 'src/templates/templates.service';
 import { PdfService } from 'src/pdf/pdf.service';
 import { CreateCurriculumQueryParamsDto } from './dto/QueryParams.dto';
+import { ImageManagementService } from 'src/image-management/image-management.service';
 
 @Injectable()
 export class CurriculumVitaeService {
   constructor(
     private readonly templateService: TemplatesService,
     private readonly pdfService: PdfService,
+    private readonly imageService: ImageManagementService,
   ) {}
 
   async create(
@@ -22,13 +24,15 @@ export class CurriculumVitaeService {
     const templateHtml = this.templateService.getTemplate(type);
     const globalCss = this.templateService.getCss('GlobalStyles');
 
-    //Convert img to base64
-    const srcImg = picture.buffer.toString('base64');
+    //Transform image to square size
+    const modifiedPicture = await this.imageService.toAvatarSize(
+      picture.buffer,
+    );
 
     const pdf = await this.pdfService.createPdf<DataTemplate>({
       data: MapperTemplateData.FromDtoToToDataTemplate(
         createCurriculumVitaeDto.data,
-        srcImg,
+        modifiedPicture.toString('base64'),
       ),
       html: templateHtml,
       css: globalCss,
