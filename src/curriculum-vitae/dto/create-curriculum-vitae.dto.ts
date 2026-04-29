7fe086ce5557cf6f7b60ae52248c959c50780efe
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-04-29 15:19:48 -0600

    refactor: Comment added to know how data is transformed in the main DTO.

--- src/curriculum-vitae/dto/create-curriculum-vitae.dto.ts
+++ src/curriculum-vitae/dto/create-curriculum-vitae.dto.ts
@@ -3,6 +3,7 @@ import { CurriculumFormDataDto } from './curriculum-form-data.dto';
 import { ValidateNested } from 'class-validator';
 
 export class CreateCurriculumVitaeDto {
+  /** Property transformed into a Dto class to receive a form-data key as json value and validate it */
   @Transform(
     ({ value }): CurriculumFormDataDto => {
       return plainToInstance(
